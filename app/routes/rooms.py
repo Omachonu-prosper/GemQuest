c559da7c35bd4e52a829d38cc9a12062f295bcb5
Omachonu Prosper <omachonucodes@gmail.com>
2024-07-06 01:22:42 +0100

    Rewrite waitroom logic

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -20,7 +20,7 @@ class RoomManager:
             print('room not found')
             return False
         
-        rooms[room_id]['connections'][username] = websocket
+        rooms[room_id]['connections'].append((username, websocket))
         return True
 
     def disconnect(self, websocket: WebSocket, room_id: str):
@@ -29,9 +29,13 @@ class RoomManager:
     # async def send_personal_message(self, message: str, websocket: WebSocket):
     #     await websocket.send_text(message)
 
-    async def broadcast(self, room_id: str, message: str):
+    async def broadcast_json(self, room_id: str, data: dict):
         for connection in rooms[room_id]['connections']:
-            await connection.send_text(message)
+            await connection[1].send_json(data)
+
+    async def close_room(self, room_id: str):
+        for connection in rooms[room_id]['connections']:
+            await connection[1].close(reason='Room Closed')
 
 
 @router.post('/gameroom/create',  status_code=201)
@@ -43,7 +47,7 @@ async def create_gameroom_route(room_details: RoomDetails):
         'room_id': room_id,
         'game_started': False,
         'no_of_questions': room_details.no_of_questions,
-        'connections': {}
+        'connections': []
     }
 
     print(rooms)
@@ -53,26 +57,43 @@ async def create_gameroom_route(room_details: RoomDetails):
     }
 
 
-room_manager = RoomManager()
-@router.websocket('/gameroom/{room_id}')
-async def gameroom_socket(websocket: WebSocket, room_id: str):
+waitingroom_manager = RoomManager()
+gameroom_manager = RoomManager()
+
+@router.websocket('/waitingroom/{room_id}')
+async def waitingroom_socket(websocket: WebSocket, room_id: str):
     username = generate_username()[0]
-    connect = await room_manager.connect(websocket, room_id, username)
+    connect = await waitingroom_manager.connect(websocket, room_id, username)
     if connect:
-        print(rooms)
-        await websocket.send_json(
-            {
-                'username': username,
-                'user_id': str(uuid4())
-            }
-        )
         try:
-            await websocket.send_text('Welcome to the game')
             while True:
-                data = await websocket.receive_text()
-                await room_manager.broadcast(f"Client says: {data}")
+                data = await websocket.receive_json()
+                user_role = data.get('user_role')
+                action = data.get('action')
+                if user_role.lower() == 'admin' and action.lower() == 'start_game':
+                    await waitingroom_manager.broadcast_json(
+                        room_id, {
+                            'username': username,
+                            'room_id': room_id,
+                            'action': 'start_game'
+                        }
+                    )
+                    await waitingroom_manager.close_room(room_id)
         except WebSocketDisconnect:
-            room_manager.disconnect(websocket)
-            await room_manager.broadcast("Someone left the game")
+            waitingroom_manager.disconnect(websocket)
     else:
         await websocket.close(reason='Room ID not found')
+
+
+@router.websocket('/gameroom/{room_id}/{username}')
+async def gameroom_socket(websocket: WebSocket, room_id: str, username: str):
+    # connect = gameroom_manager.connect(websocket, room_id, username)
+    await websocket.accept()
+    # if connect:
+    print(rooms)
+    await websocket.send_text('Welcome to the game')
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(data)
+    # else:
+        # await websocket.close(reason='Room ID not found')
