4975785d2b1199338256ffb81493798509fd2ddc
Omachonu Prosper <omachonucodes@gmail.com>
2024-07-11 15:15:47 +0100

    Refactor room connection logic

--- app/utils/room_manager.py
+++ app/utils/room_manager.py
@@ -6,22 +6,39 @@ class RoomManager:
     def __init__(self) -> None:
         self.rooms = dict()
 
-    async def connect(self, websocket: WebSocket, room_id: str, username: str) -> bool:
-        try:
-            await websocket.accept()
-            self.rooms[room_id].append(websocket)
-            room = await db.rooms.find_one_and_update(
-                {'room_id': room_id},
-                {'$set': {f'users.{username}': {
-                    'status': 'connected',
-                    }}
-                }
-            )
-            if not room:
-                return False
-            return True
-        except KeyError:
+
+    async def connect(
+            self, 
+            websocket: WebSocket,
+            room_id: str, 
+            username: str,
+            game_started: bool
+        ) -> bool:
+        """
+        Initiate a client connect to a room
+
+        - websocket: The socket connection instance
+        - room_id: The id of the room the user wants to join
+        - username: The username generated for the client
+        - game_started: Boolean value that will be True if the game has started 
+            (If this is false the client is added to a wait room and if true the client is added to a game room)
+        """
+        await websocket.accept()
+        room_in_memory = self.rooms.get(room_id)
+        if room_in_memory is None:
             return False
+        
+        room_in_memory.append(websocket)
+        room_in_db = await db.rooms.find_one_and_update(
+            {'room_id': room_id, 'game_started': game_started},
+            {'$set': {f'users.{username}': {
+                'status': 'connected',
+                }}
+            }
+        )
+        if not room_in_db:
+            return False
+        return True
 
 
     async def disconnect(self, websocket: WebSocket, room_id: str, username: str):
