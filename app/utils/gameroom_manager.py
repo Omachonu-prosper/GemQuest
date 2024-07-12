9af4864ee3de3a9c46643e50186bd694427e00c3
Omachonu Prosper <omachonucodes@gmail.com>
2024-07-12 08:31:59 +0100

    Create an end game method

--- app/utils/gameroom_manager.py
+++ app/utils/gameroom_manager.py
@@ -1,6 +1,20 @@
 from app.utils.room_manager import RoomManager
+from app.utils.db import db
 
 
 class GameroomManager(RoomManager):
     def __init__(self) -> None:
         super().__init__()
+
+    async def end_game(self, room_id: str):
+        room = self.rooms.get(room_id)        
+        if room:
+            for connection in room:
+                await connection.close(reason='Game over')
+            
+            await db.rooms.update_one(
+                {'room_id': room_id, 'game_state': 'game_started'},
+                {'$set': {'game_state': 'game_ended'}}
+            )
+            del self.rooms[room_id]
+        print(self.rooms)
