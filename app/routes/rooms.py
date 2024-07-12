2f7c0c7561762d406d198e71c4cbe37de0839381
Omachonu Prosper <omachonucodes@gmail.com>
2024-07-12 07:49:28 +0100

    Create scafold for gameflow operations

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from random_username.generate import generate_username
 
 from app.utils import waitroom_manager
+from app.utils import gameroom_manager
 from app.utils.room_details import RoomDetails
 from app.utils.db import db
 
@@ -63,9 +64,11 @@ async def start_game(room_id: str):
 @router.websocket('/waitroom/{room_id}')
 async def waitroom_socket(websocket: WebSocket, room_id: str):
     username = generate_username()[0]
+    # game_started is false so the client gets addded to a waitroom
     connect = await waitroom_manager.connect(websocket, room_id, username, False)
     if connect:
         try:
+            # Send the full list of users in the room at the point of entry
             users = await db.rooms.find_one({'room_id': room_id,}, {'_id': 0, 'users': 1})
             await waitroom_manager.send_json(websocket, {
                 'users': users.get('users'),
@@ -73,7 +76,7 @@ async def waitroom_socket(websocket: WebSocket, room_id: str):
             })
             await waitroom_manager.broadcast_json(room_id, {
                 'message': f'{username} joined',
-                'action': 'join_chat',
+                'action': 'join_room',
                 'username': username,
             }, exclude=websocket)
 
@@ -82,26 +85,48 @@ async def waitroom_socket(websocket: WebSocket, room_id: str):
                 data = await websocket.receive_text()
                 # Sit tight and wait for the game to start
         except WebSocketDisconnect:
-            print('disconnecting')
             await waitroom_manager.disconnect(websocket, room_id, username)
             await waitroom_manager.broadcast_json(room_id, {
                 'message': f"{username} left",
-                'action': 'exit_chat',
+                'action': 'exit_room',
                 'username': username
             }, exclude=websocket)
     else:
-        await websocket.close(reason='Room ID not found')
+        await websocket.close(reason='Room not found')
 
 
 @router.websocket('/gameroom/{room_id}/{username}')
 async def gameroom_socket(websocket: WebSocket, room_id: str, username: str):
-    # connect = waitroom_manager.connect(websocket, room_id, username)
-    await websocket.accept()
-    # if connect:
-    # print(gameroom_manager.rooms)
-    await websocket.send_text('Welcome to the game')
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(data)
-    # else:
-        # await websocket.close(reason='Room ID not found')
+    # game_started is false so the client gets addded to a gameroom
+    connect = gameroom_manager.connect(websocket, room_id, username, True)
+    if connect:
+        try:
+            while True:
+                data = await websocket.receive_json()
+                # Wait for a request to generate a new question
+                if data.get('moderator') and data.get('action') == 'gen_question':
+                    # Call the question generation function
+                    # Broadcast question
+                    # Store question details
+                    pass
+
+                # Wait for a client to provide the answer to a question
+                if data.get('action') == 'ans_question':
+                    # Evaluate answer
+                    # Send evaluation
+                    # Store answer
+                    pass
+
+                if data.get('moderator') and data.get('action') == 'game_over':
+                    # Braodcast leaderboard
+                    pass
+                            
+        except WebSocketDisconnect:
+            await gameroom_manager.disconnect(websocket, room_id, username)
+            await gameroom_manager.broadcast_json(room_id, {
+                'message': f"{username} left",
+                'action': 'exit_room',
+                'username': username
+            }, exclude=websocket)
+    else:
+        await websocket.close(reason='Room not found')
