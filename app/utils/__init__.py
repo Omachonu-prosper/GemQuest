b578df962420b8c2ab52b158267a9ff6bf1476e0
Omachonu Prosper <omachonucodes@gmail.com>
2024-07-12 07:48:13 +0100

    Greate a gameroom manager

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,5 @@
 from app.utils.waitroom_manager import WaitroomManager
+from app.utils.gameroom_manager import GameroomManager
 
 waitroom_manager = WaitroomManager()
+gameroom_manager = GameroomManager()
---
+++ app/utils/gameroom_manager.py
@@ -0,0 +1,6 @@
+from app.utils.room_manager import RoomManager
+
+
+class GameroomManager(RoomManager):
+    def __init__(self) -> None:
+        super().__init__()
